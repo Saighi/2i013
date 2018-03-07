1f8271be3b6f341a75b61230e9b77750d8784e8b
GRASSIN <3670720@ppti-14-303-01.ufr-info-p6.jussieu.fr>
2018-03-07 13:20:02 +0100

    nouveau fichier : Cours3.pptx 	nouveau fichier : Cours_ExplorationLocale.pdf 	nouveau fichier : Othello/Joueurs/joueur_NegaMax.py 	nouveau fichier : Othello/Joueurs/joueur_NegaMax.pyc

---
+++ Othello/Joueurs/joueur_NegaMax.py
@@ -0,0 +1,94 @@
+import sys
+sys.path.append("../..")
+import game
+
+joueur = 0
+enemi = 0
+precedent = 0
+LIGNESIZE = 6
+PROFONDEUR = 1
+
+def saisieCoup(jeu):
+	""" jeu -> coup
+	Retourne un coup a jouer aleatoire 
+	"""
+	global joueur
+	global enemi
+	joueur = jeu[1]
+	
+	enemi=1 if joueur == 2 else 2
+	
+	maxi=-10000
+	imax=0
+	
+	for i in range(len(jeu[2])):
+	
+		score = saisieCoupSimuNegaBeta(0, game.getCopieJeu(jeu), jeu[2][i])
+	
+		if score > maxi:
+		
+			maxi=score
+			imax=i
+		
+		
+	return jeu[2][imax]
+		
+	
+def saisieCoupSimuNegaBeta(profondeur,jeu,coup):
+	""" jeu -> coup
+	Retourne un coup a jouer aleatoire 
+	"""
+	global enemi
+	global PROFONDEUR
+	
+	ScorePrecedent = jeu[4]
+	game.joueCoup(jeu,coup)
+	valides = game.getCoupsValides(jeu)
+
+	if profondeur == PROFONDEUR or game.finJeu(jeu):
+	
+		if profondeur % 2 == 0:
+			return eval(jeu,ScorePrecedent)
+		else:
+			return -eval(jeu,ScorePrecedent)
+	
+	else:
+	
+		global precedent
+		imax=0
+		maxi=-1000
+	
+		for i in range(len(jeu[2])):
+	
+			score = saisieCoupSimuNegaBeta(profondeur+1, game.getCopieJeu(jeu), jeu[2][i])
+			
+			if score >= precedent:
+			
+				return calcul(score,(jeu[4][joueur-1]-ScorePrecedent[joueur-1] - jeu[4][enemi-1] - ScorePrecedent[enemi-1]))
+	
+			elif score > maxi:
+		
+				maxi=score
+			
+		precedent = calcul(maxi,(jeu[4][joueur-1]-ScorePrecedent[joueur-1] - jeu[4][enemi-1] - ScorePrecedent[enemi-1]))
+		return precedent
+		
+				
+		
+	
+def eval(jeu,ScorePrecedent):
+
+	diff = (jeu[4][joueur-1]-jeu[4][enemi-1])
+	gain = (jeu[4][joueur-1]-ScorePrecedent[joueur-1] - jeu[4][enemi-1] - ScorePrecedent[enemi-1])
+	pond=0.1
+	
+
+	return gain
+
+def calcul(score,gain):
+
+	return gain - score
+	
+		
+			
+				 
